e434e3426431dd059e82fc2bad3813ed6c336ef2
Dana Ng <ngdana1@gmail.com>
2023-03-20 19:45:47 -0700

    Fix adding sprite assets

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -7,9 +7,9 @@ export default class Preloader extends Phaser.Scene {
 
     // preload textures
     preload() {
-        
+
         this.load.spritesheet('sokoban', 'textures/sokoban_tilesheet.png', { frameWidth: 64 })
-        
+
         // load sprites
         this.load.image('burger', 'textures/burger.png')
         this.load.image('croissant', 'textures/croissant.png')
@@ -21,23 +21,23 @@ export default class Preloader extends Phaser.Scene {
     create() {
 
         // create images to render the sprites
-        let burger = this.add.image(400, 500, 'burger')
-        let croissant = this.add.image(400, 500, 'croissant')
-        let donutSprinkles = this.add.image(400, 500, 'donutSprinkles')
-        let sushiSalmon = this.add.image(400, 500, 'sushiSalmon')
-        let taco = this.add.image(400, 500, 'taco')
+        this.add.image(400, 500, 'burger')
+        this.add.image(400, 500, 'croissant')
+        this.add.image(400, 500, 'donutSprinkles')
+        this.add.image(400, 500, 'sushiSalmon')
+        this.add.image(400, 500, 'taco')
 
 
         // create animations of the spirite
         // down
         this.anims.create({
             key: 'down-idle',
-            frames: [{key: 'sokoban', frame: 52}]
+            frames: [ { key: 'sokoban', frame: 52 } ]
         })
 
         this.anims.create({
             key: 'down-walk',
-            frames: this.anims.generateFrameNumbers('sokoban', {start: 52, end: 54} ),
+            frames: this.anims.generateFrameNumbers('sokoban', { start: 52, end: 54 }),
             frameRate: 10,
             repeat: -1,
         })
@@ -45,12 +45,12 @@ export default class Preloader extends Phaser.Scene {
         // Up
         this.anims.create({
             key: 'up-idle',
-            frames: [{key: 'sokoban', frame: 55}]
+            frames: [ { key: 'sokoban', frame: 55 } ]
         })
 
         this.anims.create({
             key: 'up-walk',
-            frames: this.anims.generateFrameNumbers('sokoban', {start: 55, end: 57} ),
+            frames: this.anims.generateFrameNumbers('sokoban', { start: 55, end: 57 }),
             frameRate: 10,
             repeat: -1,
         })
@@ -59,12 +59,12 @@ export default class Preloader extends Phaser.Scene {
 
         this.anims.create({
             key: 'left-idle',
-            frames: [{key: 'sokoban', frame: 81}]
+            frames: [ { key: 'sokoban', frame: 81 } ]
         })
 
         this.anims.create({
             key: 'left-walk',
-            frames: this.anims.generateFrameNumbers('sokoban', {start: 81, end: 83} ),
+            frames: this.anims.generateFrameNumbers('sokoban', { start: 81, end: 83 }),
             frameRate: 10,
             repeat: -1,
         })
@@ -74,12 +74,12 @@ export default class Preloader extends Phaser.Scene {
 
         this.anims.create({
             key: 'right-idle',
-            frames: [{key: 'sokoban', frame: 78}]
+            frames: [ { key: 'sokoban', frame: 78 } ]
         })
 
         this.anims.create({
             key: 'right-walk',
-            frames: this.anims.generateFrameNumbers('sokoban', {start: 78, end: 80} ),
+            frames: this.anims.generateFrameNumbers('sokoban', { start: 78, end: 80 }),
             frameRate: 10,
             repeat: -1,
         })
